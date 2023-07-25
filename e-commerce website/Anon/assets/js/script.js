7b0aeb5bb849daadef68df6635f372389babbe0c
alloys <aloise148@gmail.com>
2023-07-25 22:04:04 +0300

    all files for the e-comerce website

---
+++ e-commerce website/Anon/assets/js/script.js
@@ -0,0 +1,38 @@
+//copyright michigan products
+
+/* the use of this script is controlled by michigan products*/
+
+'use strict';
+
+//modal variables
+
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+//modal function
+
+const modalCloseFunc = function () {
+    modal.classList.add('closed');
+}
+//modal eventListener
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+//accordion variables
+const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
+const accordion = document.querySelectorAll('[data-accordion]');
+for (let i = 0; i < accordionBtn.length; i++){
+    accordionBtn[i].addEventListener('click', function(){
+        const clickedBtn = this.nextElementSibling.classList.contains('active');
+        for(let i = 0; i < accordion.length; i++){
+            if(clickedBtn) break;
+            if( accordion[i].classList.contains('active')){
+                accordion[i].classList.remove('active');
+                accordionBtn[i].classList.remove('active');
+            }
+        }
+        this.nextElementSibling.classList.toggle('active');
+        this.classList.toggle('active');
+    })
+}
